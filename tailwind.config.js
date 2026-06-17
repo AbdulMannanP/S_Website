9ade488482e837023fed8324dbb6db8390f10b4e
Salmaan <salmaan@example.com>
2026-06-17 22:50:11 +0530

    fix: define process.env.NODE_ENV in esbuild to prevent React crashing in browser on Render

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./frontend/**/*.{html,js}"],
+  content: ["./frontend/**/*.{html,js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
